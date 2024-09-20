f8ce387d9b08f4b9e3683cb53b845a95262828dd
stipcorimaya <scorimayac@unjbg.edu.pe>
2024-09-20 15:51:23 -0500

    seleccion binaria

--- InsercionDir.cpp
+++ InsercionDir.cpp
@@ -14,6 +14,28 @@ int InsercionDir(int A[],int b){
 	}
 }
 
+int InsercionBin(int A[],int b){
+	for(int i=1;i<b;i++){
+		int aux = A[i];
+		int izq = 1;
+		int der = i - 1;
+		while(izq<=der){
+			int m = (izq + der)/2;
+			if(aux<A[m]){
+				der = m - 1;
+			}else{
+				izq = m + 1;
+			}
+		}
+		int j = i - 1;
+		while(j>=izq){
+			A[j+1] = A[j];
+			j = j - 1;
+		}
+		A[izq] = aux;
+	}
+}
+
 int main(){
 	int cant;
 	cout<<"ingrese la cantidad de numeros: ";
@@ -23,7 +45,7 @@ int main(){
 		cout<<"num "<<m+1<<": ";
 		cin>>arreglo[m];
 	}
-	InsercionDir(arreglo , cant);
+	InsercionBin(arreglo , cant);
 	
 	cout<<" su forma ordenada es: "<<endl;
 	for(int n=0; n < cant ; n++){
